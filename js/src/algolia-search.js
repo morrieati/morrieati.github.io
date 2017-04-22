946a56624ffd0b8afb639536645992d59e867e28
Morris Liu <morrislew@outlook.com>
2017-04-22 21:19:27 +0800

    Site updated: 2017-04-22 21:19:21

--- js/src/algolia-search.js
+++ js/src/algolia-search.js
@@ -37,9 +37,15 @@ $(document).ready(function () {
       hitsPerPage: algoliaSettings.hits.per_page || 10,
       templates: {
         item: function (data) {
+          var link = data.permalink ? data.permalink : (CONFIG.root + data.path);
           return (
+<<<<<<< HEAD
             '<a href="' + CONFIG.root + data.path + '" class="algolia-hit-item-link">' +
             data._highlightResult.title.value +
+=======
+            '<a href="' + link + '" class="algolia-hit-item-link">' +
+              data._highlightResult.title.value +
+>>>>>>> 3b64c7146ad1e0cd8b4a45140b42f7a0404b28d6
             '</a>'
           );
         },
@@ -98,14 +104,21 @@ $(document).ready(function () {
 
   $('.popup-trigger').on('click', function (e) {
     e.stopPropagation();
+<<<<<<< HEAD
     $('body').append('<div class="popoverlay">').css('overflow', 'hidden');
     $('.popup').show();
+=======
+    $('body')
+      .append('<div class="search-popup-overlay algolia-pop-overlay"></div>')
+      .css('overflow', 'hidden');
+    $('.popup').toggle();
+>>>>>>> 3b64c7146ad1e0cd8b4a45140b42f7a0404b28d6
     $('#algolia-search-input').find('input').focus();
   });
 
   $('.popup-btn-close').click(function () {
     $('.popup').hide();
-    $('.popoverlay').remove();
+    $('.algolia-pop-overlay').remove();
     $('body').css('overflow', '');
   });
 
