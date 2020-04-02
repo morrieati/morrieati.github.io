b364f2ad8934ec308da69dea3b9642331e298d9e
Morris Liu <lym@everxyz.com>
2020-04-02 12:25:58 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4032c92dcaa8c93baea2a31289538d93.js"
+  "/precache-manifest.18c11fd56b3faf898c07335034ee02f0.js"
 );
 
 self.addEventListener('message', (event) => {
